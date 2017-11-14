7bf19d354540e2411b5264d05bdd70bff6292be7
iyegoroff <iegoroff@gmail.com>
2017-11-15 00:31:35 +0300

    grad

--- ReactAndroid/src/main/java/com/facebook/react/views/text/ReactTextShadowNode.java
+++ ReactAndroid/src/main/java/com/facebook/react/views/text/ReactTextShadowNode.java
@@ -28,6 +28,7 @@ import com.facebook.yoga.YogaMeasureMode;
 import com.facebook.yoga.YogaMeasureOutput;
 import com.facebook.yoga.YogaNode;
 import javax.annotation.Nullable;
+import com.facebook.react.uimanager.annotations.ReactProp;
 
 /**
  * {@link ReactBaseTextShadowNode} concrete class for anchor {@code Text} node.
