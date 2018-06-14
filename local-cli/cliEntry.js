7768fc3beb89fa5a9b68b34f82523e9044f33438
iyegoroff <iegoroff@gmail.com>
2018-06-14 09:05:44 +0300

    Update cliEntry.js

--- local-cli/cliEntry.js
+++ local-cli/cliEntry.js
@@ -36,6 +36,10 @@ const handleError = (err) => {
   console.error();
   console.error(err.message || err);
   console.error();
+  if (err.stack) {
+    console.error(err.stack);
+    console.error();
+  }
   process.exit(1);
 };
 
